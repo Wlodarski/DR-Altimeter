3378ca995fb1d29e90751f6744f92216fd246c99
Wlodarski <Wlodarski@users.noreply.github.com>
2020-03-03 10:42:13 -0500

    streamlined gettext translation

--- src/translation.py
+++ src/translation.py
@@ -10,10 +10,13 @@ class Translation:
     def __init__(self):
         self.gettext = self.temporary_
 
-    def set_lang(self, clp, args):
+    def __call__(self, text):
+        return self.gettext(text)
+
+    def set_lang(self, clp):
         current_locale, encoding = getdefaultlocale()
         lang = ''
-        if args.lang in clp.all_lang:
+        if clp.args.lang in clp.all_lang:
             lang = clp.args.lang
         chosen_lang = gettext.translation('DR-Altimeter',
                                           localedir=clp.localedir,
@@ -21,3 +24,4 @@ class Translation:
                                           fallback=True)
         chosen_lang.install()
         self.gettext = chosen_lang.gettext
+        return self.gettext
